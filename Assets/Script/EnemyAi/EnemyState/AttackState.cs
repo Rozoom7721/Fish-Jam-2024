8b061be9496fd24376781f86ef96fc7e5ccb92a1
Rozoom7721 <rozumpl2@gmail.com>
2024-04-07 19:45:37 +0200

    Create basic Ai in game

---
+++ Assets/Script/EnemyAi/EnemyState/AttackState.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackState : StateBase
+{
+    double gold;
+    double unitCost;
+    string unitName;
+    public override void OnEnter(AiStateMenager aiState, BattleSystem battle)
+    {
+        aiState.SelectUnit();
+        gold = battle.enemyGold;
+        unitName = aiState.unitName;
+        switch (unitName)
+        {
+            case "healer":
+                unitCost = battle.enemyHealerCost; break;
+            case "tank":
+                unitCost = battle.enemyTankCost; break;
+            case "range":
+                unitCost = battle.enemyRangeCost; break;
+            case "melee":
+                unitCost = battle.enemyMeleeCost; break;
+            default:
+                break;
+        }
+    }
+
+    public override void OnExit(AiStateMenager aiState, BattleSystem battle)
+    {
+
+    }
+
+    public override void OnFixedUpdate(AiStateMenager aiState, BattleSystem battle)
+    {
+
+    }
+
+    public override void OnUpdate(AiStateMenager aiState, BattleSystem battle)
+    {
+
+    }
+}
