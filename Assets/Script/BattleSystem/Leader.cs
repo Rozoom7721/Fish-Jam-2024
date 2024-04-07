d53d6bf80214616967e1011617454fb3705e2375
Bartek Nowak <barteknowak31l@gmail.com>
2024-04-07 20:13:09 +0200

    leader sprite is updating properly in battle

---
+++ Assets/Script/BattleSystem/Leader.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leader : MonoBehaviour
+{
+
+    public bool isPlayer;
+    BattleSystem battleSystem;
+    SpriteRenderer leaderSprite;
+
+    private void Start()
+    {
+
+        battleSystem = GameObject.FindAnyObjectByType<BattleSystem>();
+        leaderSprite = GetComponent<SpriteRenderer>();
+
+
+        Debug.LogWarning("elo");
+
+
+    }
+
+    private void Update()
+    {
+        if(leaderSprite.sprite == null)
+        {
+            if (isPlayer)
+            {
+                leaderSprite.sprite = battleSystem.playerFraction.fractionPassives.leaderSprite;
+            }
+            else
+            {
+                leaderSprite.sprite = battleSystem.enemyFraction.fractionPassives.leaderSprite;
+
+            }
+        }
+    }
+
+
+}
